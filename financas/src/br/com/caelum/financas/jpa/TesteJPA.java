bc5a2d396b86a3a5a94016657a91307bac68a66f
ildojunior4 <ildozacarias@hotmail.com.br>
2015-04-29 07:36:26 -0300

    Status Management by the EntityManager
    Using find() , merger(), persist() !

---
+++ financas/src/br/com/caelum/financas/jpa/TesteJPA.java
@@ -0,0 +1,32 @@
+package br.com.caelum.financas.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+	
+
+
+import br.com.caelum.financas.model.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteJPA {
+
+    public static void main(String[] args) {
+    	double inicio = System.currentTimeMillis();
+        Conta conta = new Conta();
+        conta.setTitular("Nico Steppat");
+        conta.setBanco("Itau");
+        conta.setAgencia("3456");
+        conta.setNumero("918273-4");
+                    
+        EntityManager em = new JPAUtil().getEntityManager();
+        em.getTransaction().begin();
+
+        em.persist(conta);
+
+        em.getTransaction().commit();
+        em.close();
+        double fim = System.currentTimeMillis();
+        System.out.println("Executado em: " + (fim - inicio)/1000 + "s");           
+    }
+}
